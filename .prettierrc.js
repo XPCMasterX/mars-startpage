b0f44569366906712259141e2e445a53fb4bd505
powerlynx <shashi@wescheduleit.co.uk>
2021-02-02 12:13:10 +0000

    Commit to github

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/* Prettier Config */
+
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+};
---
+++ src/App.svelte
@@ -0,0 +1,116 @@
+<script>
+	import config from '../config.yaml';
+
+	function evalWelcome(string, nameConstant) {
+		return string.replace(nameConstant, config.name);
+	}
+
+	function evalSearchEngine(searchEngine) {
+		switch(searchEngine) {
+			case 'DuckDuckGo':
+				return 'https://duckduckgo.com/search';
+				break;
+			case 'Google':
+				return 'https://google.com/search'
+				break;
+			case 'Bing':
+				return 'https://bing.com/search'
+				break;
+			default:
+				return searchEngine;
+		}
+	}
+</script>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+	/* Configure the colours here */
+	:root {
+		/* Colours */
+		--gradient-start: #ff005d;
+		--gradient-end: #ff8b26;
+		--background-color: #181b1e;
+
+		/* Sizes */
+		--text-font-size: 150px;
+	}
+
+	p {
+		font-family: Roboto;
+		font-size: var(--text-font-size);
+
+		/* Get the Gradient */
+		background-image: linear-gradient(45deg, var(--gradient-start), var(--gradient-end));
+		background-size: 100%;
+		background-repeat: repeat;
+	    -webkit-background-clip: text;
+	    -webkit-text-fill-color: transparent;
+	    -moz-background-clip: text;
+	    -moz-text-fill-color: transparent;
+	    background-clip: text;
+
+		/* Force words to be on the same line */
+		overflow: hidden;
+    	white-space: nowrap;
+
+		/* Antialiasing?*/
+		-webkit-font-smoothing: antialiased;
+	    -moz-osx-font-smoothing: grayscale;
+
+		/* Centering */
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		margin-top: 15%;
+	}
+
+	form {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		margin-top: 25%;
+	}
+
+	div {
+		position: relative;
+	}
+
+	#search {
+		border: none;
+		outline: none;
+		width: 40rem;
+		background-color: var(--background-color);
+		height: 35px;
+		border-radius: 20px;
+		text-indent: 10px;
+		color: white;
+		font-family: Roboto;
+		font-weight: bold;
+	}
+	.search-bar-wrap {
+		-webkit-border-radius: 50px;
+	  -moz-border-radius: 50px;
+	  border-radius: 50px;
+	  border: none;
+	  padding: 1rem;
+	  position: relative;
+	  background: linear-gradient(to right, #ff005d, #ff8b26);
+	  padding: 3px;
+	  display: inline-block;
+	}
+</style>
+
+
+<div>
+	<p>{evalWelcome(config.greeting, config.nameConstant)}</p>
+	<form action={evalSearchEngine(config.searchEngine)} method="get">
+		<input type="hidden" name="sitesearch" />
+		<div id="search-area">
+			<div class="search-bar-wrap">
+					<input type="text" name="q" id="search"/>
+			</div>
+	</form>
+</div>
