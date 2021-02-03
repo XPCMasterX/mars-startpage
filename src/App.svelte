da3c3e2fbc54184c85f08e25357ea4ba5ea11566
XPCMasterX <xpcmasterx007@gmail.com>
2021-02-03 09:48:14 +0000

    Added blur strength

--- src/App.svelte
+++ src/App.svelte
@@ -1,178 +1,194 @@
 <script>
-	import config from '../config.yaml';
-	import { onMount } from 'svelte';
-	import Options from './lib/Options.svelte';
+    import config from '../config.yaml';
+    import { onMount } from 'svelte';
+    import Options from './lib/Options.svelte';
 
+    let visible = true;
 
-	let visible = false;
+    function evalWelcome(string, nameConstant) {
+        return string.replace(nameConstant, config.name);
+    }
 
-	function evalWelcome(string, nameConstant) {
-		return string.replace(nameConstant, config.name);
-	}
+    function evalSearchEngine(searchEngine) {
+        switch (searchEngine) {
+            case 'DuckDuckGo':
+                return 'https://duckduckgo.com/search';
+                break;
+            case 'Google':
+                return 'https://google.com/search';
+                break;
+            case 'Bing':
+                return 'https://bing.com/search';
+                break;
+            default:
+                return searchEngine;
+        }
+    }
 
-	function evalSearchEngine(searchEngine) {
-		switch(searchEngine) {
-			case 'DuckDuckGo':
-				return 'https://duckduckgo.com/search';
-				break;
-			case 'Google':
-				return 'https://google.com/search'
-				break;
-			case 'Bing':
-				return 'https://bing.com/search'
-				break;
-			default:
-				return searchEngine;
-		}
-	}
+    function loadCSSOptions() {
+        let root = document.documentElement;
+        let cssProperties = Object.entries(config.css);
 
-	function loadCSSOptions() {
-		let root = document.documentElement;
-		let cssProperties = Object.entries(config.css);
+        for (const [key, value] of cssProperties) {
+            root.style.setProperty('--' + key, value.toString());
+        }
+    }
 
-		for (const [key, value] of cssProperties) {
-			root.style.setProperty('--' + key, value.toString())
-		}
-	}
+    function toggleVisible() {
+        visible = visible === true ? false : true;
+    }
 
-	function toggleVisible() {
-		visible = visible === true ? false : true;
-	}
-
-	onMount(async () => {
-		loadCSSOptions();
-		document.body.style['background-image'] = 'url(./background.jpg)';
-	})
-
+    onMount(async () => {
+        loadCSSOptions();
+        document.body.style['background-image'] = 'url(./background.jpg)';
+    });
 </script>
 
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
-
-	p {
-		font-family: Roboto;
-		font-size: var(--text-font-size);
-
-		/* Get the Gradient */
-		background-image: linear-gradient(45deg, var(--gradient-start), var(--gradient-end));
-		background-size: 100%;
-		background-repeat: repeat;
-	    -webkit-background-clip: text;
-	    -webkit-text-fill-color: transparent;
-	    -moz-background-clip: text;
-	    -moz-text-fill-color: transparent;
-	    background-clip: text;
-
-		/* Force words to be on the same line */
-		overflow: hidden;
-    	white-space: nowrap;
-
-		/* Antialiasing?*/
-		-webkit-font-smoothing: antialiased;
-	    -moz-osx-font-smoothing: grayscale;
-
-		/* Centering */
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		margin-top: 15%;
-	}
-
-	form {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		margin-top: 25%;
-	}
-
-	div {
-		position: relative;
-	}
-
-	#search {
-		border: none;
-		outline: none;
-		width: 40rem;
-		background-color: var(--background-color);
-		height: 35px;
-		border-radius: 20px;
-		text-indent: 10px;
-		color: white;
-		font-family: Roboto;
-		font-weight: bold;
-	}
-	.search-bar-wrap {
-		-webkit-border-radius: 50px;
-	  -moz-border-radius: 50px;
-	  border-radius: 50px;
-	  border: none;
-	  padding: 1rem;
-	  position: relative;
-	  background: linear-gradient(45deg, var(--gradient-start), var(--gradient-end));
-	  padding: 3px;
-	  display: inline-block;
-	}
-
-	img {
-		height: 32px;
-		width: 32px;
-
-		/* Vertically align it to the center */
-		padding-top: 8px;
-	}
-
-	.settings  {
-		/* Make it bigger than the icon */
-		height: 48px;
-		width: 48px;
-
-		/* Align the icon to the center */
-		text-align: center;
-
-		/* Position it at bottom left */
-		position: fixed;
-		bottom: 8px;
-		right: 8px;
-
-		/* Blur the background */
-		-webkit-backdrop-filter: blur(10px);
-        backdrop-filter: blur(10px);
-        background-color: rgba(0, 0, 0, 0.3);
-
-		/* Rounded corners */
-		border-radius: 10px;
-
-		/* Keep this here so it darkens and lightens nicely */
-		transition: 0.5s;
-	}
-
-	.settings:hover {
-		background-color: rgba(0, 0, 0, 0.1);
-	}
-</style>
-
-
 <div>
-	<p>{evalWelcome(config.greeting, config.nameConstant)}</p>
-	<form action={evalSearchEngine(config.searchEngine)} method="get">
-		<input type="hidden" name="sitesearch" />
-		<div id="search-area">
-			<div class="search-bar-wrap">
-					<input type="text" name="q" id="search"/>
-			</div>
-	</form>
+    <p>{evalWelcome(config.greeting, config.nameConstant)}</p>
+    <form action={evalSearchEngine(config.searchEngine)} method="get">
+        <input type="hidden" name="sitesearch" />
+        <div id="search-area">
+            <div class="search-bar-wrap">
+                <input type="text" name="q" id="search" />
+            </div>
+        </div>
+    </form>
 </div>
-
-
-
 {#if visible}
-	<Options />
+    <Options />
 {/if}
-
 
 <!--Icons made by Freepik from www.flaticon.com-->
 <div class="settings">
-	<img src="./settings.svg" alt="Settings" on:click={toggleVisible}>
+    <img src="./settings.svg" alt="Settings" on:click={toggleVisible} />
 </div>
+
+<div class="blur" />
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+    p {
+        font-family: Roboto;
+        font-size: var(--text-font-size);
+
+        /* Get the Gradient */
+        background-image: linear-gradient(
+            45deg,
+            var(--gradient-start),
+            var(--gradient-end)
+        );
+        background-size: 100%;
+        background-repeat: repeat;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        -moz-background-clip: text;
+        -moz-text-fill-color: transparent;
+        background-clip: text;
+
+        /* Force words to be on the same line */
+        overflow: hidden;
+        white-space: nowrap;
+
+        /* Antialiasing?*/
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        /* Centering */
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        margin-top: 15%;
+    }
+
+    form {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        margin-top: 25%;
+    }
+
+    div {
+        position: relative;
+    }
+
+    #search {
+        border: none;
+        outline: none;
+        width: 40rem;
+        background-color: var(--search-background-color);
+        height: 35px;
+        border-radius: 20px;
+        text-indent: 10px;
+        color: white;
+        font-family: Roboto;
+        font-weight: bold;
+    }
+    .search-bar-wrap {
+        -webkit-border-radius: 50px;
+        -moz-border-radius: 50px;
+        border-radius: 50px;
+        border: none;
+        padding: 1rem;
+        position: relative;
+        background: linear-gradient(
+            45deg,
+            var(--gradient-start),
+            var(--gradient-end)
+        );
+        padding: 3px;
+        display: inline-block;
+    }
+
+    img {
+        height: 32px;
+        width: 32px;
+
+        /* Vertically align it to the center */
+        padding-top: 8px;
+    }
+
+    .settings {
+        /* Make it bigger than the icon */
+        height: 48px;
+        width: 48px;
+
+        /* Align the icon to the center */
+        text-align: center;
+
+        /* Position it at bottom left */
+        position: fixed;
+        bottom: 8px;
+        right: 8px;
+
+        /* Blur the background */
+        -webkit-backdrop-filter: blur(10px);
+        backdrop-filter: blur(10px);
+        background-color: rgba(0, 0, 0, 0.3);
+
+        /* Rounded corners */
+        border-radius: 10px;
+
+        /* Keep this here so it darkens and lightens nicely */
+        transition: 0.5s;
+    }
+
+    .settings:hover {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
+
+    .blur {
+        position: fixed;
+        top: 0;
+        right: 0;
+        height: 100vh;
+        width: 100vw;
+        background-color: rgba(0, 0, 0, 0.7);
+        z-index: -1;
+        -webkit-backdrop-filter: blur(var(--blur-strength));
+        backdrop-filter: blur(var(--blur-strength));
+    }
+</style>
--- src/lib/Options.svelte
+++ src/lib/Options.svelte
@@ -1,15 +1,33 @@
 <script>
     import { fly } from 'svelte/transition';
+
+    let blurSlider = 25;
+
+    let root = document.documentElement;
+    $: root.style.setProperty('--blur-strength', blurSlider + 'px');
+    $: console.log(blurSlider);
 </script>
+
+<div class="option-container" transition:fly={{ x: -310, opacity: 1 }}>
+    <div class="option">
+        <div class="blur">
+            <p for="blurStrength" id="blurStrengthLabel">Blur Strength:</p>
+            <input
+                type="range"
+                min="1"
+                max="100"
+                bind:value={blurSlider}
+                id="blurStrength"
+            />
+        </div>
+    </div>
+</div>
 
 <style>
     .option-container {
         color: white;
         font-family: sans-serif;
-        line-height: 1.5;
         border-radius: 0;
-        height: 100vh;
-        width: 35vh;
 
         /* Blur background */
         -webkit-backdrop-filter: blur(10px);
@@ -21,45 +39,67 @@
         top: 0;
         left: 0;
 
+        height: 100vh;
+        width: 20vw;
+
         /* Give space around child elements */
         padding: 3vh;
         padding-top: 1vh;
         padding-bottom: 1vh;
 
         /* Keep this here so it darkens and lightens nicely */
-		transition: 0.5s;
+        transition: 0.5s;
     }
 
     .option {
         /* Nice looking Shadow */
-        box-shadow:
-          0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-          0 6.7px 5.3px rgba(0, 0, 0, 0.048),
-          0 12.5px 10px rgba(0, 0, 0, 0.06),
-          0 22.3px 17.9px rgba(0, 0, 0, 0.072),
-          0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-          0 100px 80px rgba(0, 0, 0, 0.12)
-        ;
+        box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+            0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+            0 12.5px 10px rgba(0, 0, 0, 0.06),
+            0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+            0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+            0 100px 80px rgba(0, 0, 0, 0.12);
 
         background-color: rgba(0, 0, 0, 0.1);
         width: 30;
         height: 30vh;
         border-radius: 10px;
+
+        height: 7vh;
+        width: 20vw;
     }
 
     .option-container:hover {
-		background-color: rgba(0, 0, 0, 0.1);
-	}
-</style>
+        background-color: rgba(0, 0, 0, 0.1);
+    }
 
-<div class="option-container" transition:fly="{{ x: -310, opacity: 1}}">
-    <div class="option">
-        <p>Dark Mode</p>
-    </div>
-    <div class="option">
-        <p>Dark Mode</p>
-    </div>
-    <div class="option">
-        <p>Dark Mode</p>
-    </div>
-</div>
+    #blurStrength {
+        -webkit-appearance: none;
+        outline: none;
+        height: 1px;
+        transform: translateY(5px);
+        position: fixed;
+        margin-top: 2.7vh;
+        margin-left: 10px;
+    }
+
+    #blurStrength::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: 20px;
+        border-radius: 50%;
+        height: 20px;
+        background: #ffffff;
+    }
+
+    #blurStrengthLabel {
+        display: inline-block;
+        font-size: 15px;
+        margin-left: 7px;
+    }
+
+    .blur {
+        position: relative;
+        margin-left: 20px;
+    }
+</style>
